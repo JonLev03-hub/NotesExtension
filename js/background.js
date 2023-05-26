41baf3e4a574c6e7d09d1b0e54dd92ba080ff26b
Jonathan Leverenz Desktop <jonlev03@gmail.com>
2023-05-26 17:52:04 -0400

    finishing touches

--- js/background.js
+++
@@ -1,7 +0,0 @@
-chrome.commands.onCommand.addListener(function (command) {
-  if (command === "_execute_browser_action") {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, { action: "openPopup" });
-    });
-  }
-});
--- js/index.js
+++ js/index.js
@@ -6,14 +6,20 @@ function loadSaved() {
   if (storedNotes && storedNoteIndex) {
     notes = JSON.parse(storedNotes);
     noteIndex = parseInt(storedNoteIndex);
-  }
-  if (notes.length == 0) {
-    notes.push({
-      title: "",
-      body: "",
-      color: "",
-      deleted: false,
-    });
+  } else {
+    localStorage.setItem(
+      "notes",
+      JSON.stringify([
+        {
+          title: "",
+          body: "",
+          color: "",
+          deleted: false,
+        },
+      ])
+    );
+    localStorage.setItem("noteIndex", "0");
+    loadSaved();
   }
 }
 function SaveLoaded() {
@@ -25,7 +31,22 @@ function autoResize(id) {
   textArea.style.height = "auto";
   textArea.style.height = textArea.scrollHeight + 10 + "px";
 }
+function addNote() {
+  notes.push({
+    title: "",
+    body: "",
+    color: "",
+    deleted: false,
+  });
+  noteIndex = notes.length - 1;
+  updateSidebar();
+  SaveLoaded();
+  updateTextArea();
+}
 function changeJsText(id) {
+  if (notes.length == 0) {
+    addNote();
+  }
   const textArea = document.getElementById(id);
   notes[noteIndex][id] = textArea.value;
   SaveLoaded();
@@ -37,17 +58,31 @@ function autoTrim(id) {
   const textArea = document.getElementById(id);
   textArea.value = textArea.value.trimEnd();
 }
+function updateTextArea() {
+  if (noteIndex > notes.length - 1) {
+    noteIndex = 0;
+  }
+  const title = document.getElementById("title");
+  const body = document.getElementById("body");
+  title.value = notes[noteIndex] ? notes[noteIndex].title : "";
+  body.value = notes[noteIndex] ? notes[noteIndex].body : "";
+}
+
 function updateSidebar() {
+  // select item and reset inner html
   var ul = document.getElementById("notes");
   ul.innerHTML = "";
+
   for (var i = 0; i < notes.length; i++) {
     var note = notes[i];
+
+    // check if title doesnt exist or is too long and update that
     displayTitle =
       note.title.length < 25 ? note.title : note.title.substring(0, 20) + "...";
     displayTitle = displayTitle == "" ? "Untitled Note" : displayTitle;
-    console.log(note.title.length);
+
     var liHTML =
-      `<li class = "noteItem" id = ${i}>` +
+      `<li class = "noteItem ${i == noteIndex ? "focus" : ""}" id = ${i}>` +
       "<h3>" +
       displayTitle +
       "</h3>" +
@@ -55,17 +90,19 @@ function updateSidebar() {
       '<span class="material-symbols-outlined"> delete </span>' +
       "</button>" +
       "</li>";
-
     ul.innerHTML += liHTML;
   }
+
+  // add event listeners to all items
   var listItems = document.querySelectorAll(".noteItem");
   listItems.forEach(function (listItem) {
     listItem.addEventListener("click", function () {
+      // when an item name is clicked, update the noteIndex, and update the text area
       var itemId = listItem.id;
-      // Call the clicked function and pass the item ID
-      console.log(itemId);
       noteIndex = itemId;
       updateTextArea();
+      updateSidebar();
+      SaveLoaded();
     });
   });
 
@@ -73,38 +110,27 @@ function updateSidebar() {
   listItems.forEach(function (listItem) {
     listItem.addEventListener("click", function () {
       var itemId = listItem.id;
-      // Call the clicked function and pass the item ID
-      if (notes.length == 0) {
-        notes.push({
-          title: "",
-          body: "",
-          color: "",
-          deleted: false,
-        });
-      }
+      // when a delete button is clicked, remove the item, update the sidebar, and update the localstorage
       notes.splice(itemId, 1);
       updateSidebar();
       SaveLoaded();
-
-      // updateTextArea();
     });
   });
 }
-function updateTextArea() {
-  if (noteIndex > notes.length - 1) {
-    noteIndex = 0;
-  }
-  const title = document.getElementById("title");
-  const body = document.getElementById("body");
-  title.value = notes[noteIndex].title;
-  body.value = notes[noteIndex].body;
-}
+
 function init() {
+  // set variables here for them to be accessed gloally
   notes = {};
   noteIndex = 0;
+
+  // build initial view
   loadSaved();
-  updateSidebar();
   updateTextArea();
+  updateSidebar();
+
+  // add event listeners for each action
+
+  // code for all text area event listeners
   d3.select("#title").on("input", () => {
     autoResize("title");
     changeJsText("title");
@@ -120,15 +146,10 @@ function init() {
   d3.select("#body").on("change", () => {
     autoTrim("body");
   });
+
+  // add item button
   d3.select("#addItem").on("click", () => {
-    notes.push({
-      title: "",
-      body: "",
-      color: "",
-      deleted: false,
-    });
-    updateSidebar();
-    SaveLoaded();
+    addNote();
   });
 }
 init();
